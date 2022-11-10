33fe25ab43b26b495a95e502094e060542e9666a
Javier Urzua Ahumada <javierfeli.urzua@mail.udp.cl>
2022-11-10 16:03:34 -0300

    ADD:avance back v2.0

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,30 +1,49 @@
-const connection = require("../db");
+const mysql = require("mysql");
+const db_config = require("../db");
 
 const getAllProducts = (req, res) => {
   try {
+    const connection = mysql.createConnection(db_config);
+
+    //.connect es implicito si se realiza un query
+    // connection.connect(function (error) {
+    //   if (error) throw error;
+    //   return;
+    // });
+
     connection.query("SELECT * FROM product", function (error, result) {
       if (error) throw error;
-      //console.log(result);
       //el callback function permite obtener el resultado
+      //console.log(result);
+      connection.destroy();
       res.send(result);
     });
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     res.status(500);
     res.send(error);
   }
 };
 
-const searchProduct = async (req, res) => {
+const searchProduct = (req, res) => {
   try {
     const { id } = req.params;
-    const connection = await getConnection();
-    const result = await connection.query(
+
+    const connection = mysql.createConnection(db_config);
+
+    connection.query(
       "SELECT * FROM product WHERE id = ?",
-      id
+      id,
+      function (error, result) {
+        if (error) throw error;
+        //el callback function permite obtener el resultado
+        //console.log(result);
+        connection.destroy();
+        res.send(result);
+      }
     );
-    res.send(result);
   } catch (error) {
+    //console.log(error);
     res.status(500);
     res.send(error.message);
   }
--- src/db.js
+++ src/db.js
@@ -1,5 +1,3 @@
-const mysql = require("mysql");
-
 const db_config = {
   host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
   port: "3306",
@@ -8,22 +6,4 @@ const db_config = {
   database: "bsale_test",
 };
 
-const connection = mysql.createConnection(db_config);
-
-// .connect es implicito si se realiza un query
-connection.connect(function (error) {
-  if (error) throw error;
-  return;
-});
-
-connection.on("error", function (err) {
-  //console.log(err.code);
-  if (err.code == "PROTOCOL_CONNECTION_LOST") {
-    connection.connect(function (error) {
-      console.log(error);
-      return;
-    });
-  }
-});
-
-module.exports = connection;
+module.exports = db_config;
