26f652b9aacdcbecf6006863ef34cedf0c227645
Javier Urzua Ahumada <javierfeli.urzua@mail.udp.cl>
2022-11-12 01:35:17 -0300

    PREP: sv preparation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const routes = require("./routes/routes");
+const cors = require("cors");
 
 //settings
 app.set("port", process.env.PORT || 3000);
 
 //middlewares
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
